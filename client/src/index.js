66392a3c82938ce6a7438a3f500507efb2315475
raximvodil <raximvodilrozbek@gmail.com>
2021-11-23 16:00:50 +0500

    11-block 8-video finished

--- client/src/index.js
+++ client/src/index.js
@@ -2,11 +2,26 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
 import rootReducer from "./redux/rootReducer";
 
-const store = createStore(rootReducer);
+// function loggerMiddleware(store) {
+//   return function (next) {
+//     return function (action) {
+//       const result = next(action);
+//       console.log("Middleware", store.getState());
+//       return result;
+//     };
+//   };
+// }
+
+const loggerMiddleware = (store) => (next) => (action) => {
+  console.log("Middleware", store.getState());
+  return next(action);
+};
+
+const store = createStore(rootReducer, applyMiddleware(loggerMiddleware));
 
 const app = (
   <Provider store={store}>
